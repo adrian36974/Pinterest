42a141bcea652b75532e622d7bf04817e637f308
Adrian Alejandro Sandoval Perez <adrian@MacBook-Pro-de-Adrian.local>
2019-02-07 21:06:38 -0600

    Firebase and Login

--- Pinterest/ViewController.swift
+++ Pinterest/ViewController.swift
@@ -7,14 +7,143 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(r: 201, g: 34, b: 40)
+        
+        let imageName = "pig"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 55, y: 140, width: 300, height: 300)
+        view.addSubview(imageView)
+        
+        //add subview
+        view.addSubview(inputContainerView)
+        view.addSubview(firstButton)
+        inputContainerView.addSubview(nameTextField)
+        inputContainerView.addSubview(emailTextField)
+        inputContainerView.addSubview(passwordTextField)
+        
+        
+        //constraints
+        // constraints for input
+        
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        
+        
+        
+        firstButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 20).isActive = true
+        firstButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        firstButton.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        firstButton.rightAnchor.constraint(equalTo: inputContainerView.rightAnchor).isActive = true
+        
+        nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
+        emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    let emailTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Correo Electrónico"
+        tf.backgroundColor = .white
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 20))
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    let passwordTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Contraseña"
+        tf.backgroundColor = .white
+        tf.isSecureTextEntry = true
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 20))
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    let nameTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Nombre"
+        tf.backgroundColor = .white
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 20))
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    let inputContainerView : UIView =  {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let firstButton : UIButton = {
+        let ub = UIButton()
+        ub.backgroundColor =  UIColor(r: 235, g: 101, b: 90)
+        ub.setTitle("Registrarse", for: .normal)
+        ub.translatesAutoresizingMaskIntoConstraints = false
+        ub.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
+        return ub
+    }()
+    
+    @objc func handleButton(){
+        print("Hola mundo")
+        if let email = emailTextField.text, let pass = passwordTextField.text, let name = nameTextField.text{
+        print(email)
+        print(pass)
+            Auth.auth().createUser(withEmail: email, password: pass) { (data:AuthDataResult?, error) in
+                var user = data?.user
+                if error != nil {
+                    print(error)
+                }
+                
+                //succesful
+                var ref = Database.database().reference(fromURL: "https://pinterest-tecmi.firebaseio.com/")
+                
+                if let uid = user?.uid{
+                
+                    var usersRef = ref.child("users").child(uid)
+                    usersRef.updateChildValues(["name" : name, "email" : email, "password": pass])
+                }
+            }
+        }
+    }
+    
 }
 
+
+extension UIColor {
+    
+    convenience init(r:CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
